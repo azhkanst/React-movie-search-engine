110aebce85b143b749dba154c0ddebc5c78999f8
Gattskii <117891948+Gattskii@users.noreply.github.com>
2025-07-05 22:03:36 +0700

    Update Home.jsx

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,22 +1,29 @@
 import MovieCard from "../components/MovieCard"
+import {useState} from "react"
 
 function Home() {
+
+    const [searchQuery, setSearchQuery] = useState("");
+
     const movies = [
         {id: 1, title: "Atonement", release_date: "2007"},
         {id: 2, title: "Movie2", release_date: "2024"},
         {id: 3, title: "Movie3", release_date: "2004"},
     ];
 
-    function onSubmit() {
-        
+    function handleSearch() {
+        e.preventDefault()
+        alert(searchQuery);
     }
 
     return <div className="home">
-        <form onSubmit={onSubmit} className="search-form">
+        <form onSubmit={handleSearch} className="search-form">
             <input 
                 type="text" 
                 placeholder="search for movies.." 
                 className="search-input"
+                value={searchQuery}
+                onChange={(e) => setSearchQuery(e.target.value)}
             />
             <button type="submit" className="search-button">Search</button>
         </form>
